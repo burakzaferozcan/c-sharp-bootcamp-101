d4442480f96b48f5f818401650ea2ed1852e4a20
burak zafer ozcan <ozcanburakzafer@gmail.com>
2024-10-15 17:37:27 +0300

    Print commands created.

--- 00-main_subjects/00-main_subjects/Program.cs
+++ 00-main_subjects/00-main_subjects/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
+            #region Yazdırma Komutları
+            //! satır sonunda imleç aşağı geçer
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            //! satır sonunda imleç olduğu yerde kalır
+            Console.Write("Hello, World!");
+            Console.Write("Hello, World!");
 
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine("***** Yemek Kategorileri *****");
+            Console.WriteLine();
+            Console.WriteLine("1-Çorbalar");
+            Console.WriteLine("2-Ana Yemekler");
+            Console.WriteLine("3-Soğuk Başlangıçlar");
+            Console.WriteLine("4-Salatalar");
+            Console.WriteLine("5-Tatlılar");
+            Console.WriteLine("6-İçecekler");
+            Console.WriteLine();
+            Console.WriteLine("***** Yemek Kategorileri *****");
+            #endregion
+
+            Console.Read();
         }
     }
 }
