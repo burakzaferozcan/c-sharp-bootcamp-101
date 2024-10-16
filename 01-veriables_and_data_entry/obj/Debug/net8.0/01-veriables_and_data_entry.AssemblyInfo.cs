e3419bbf2a69b8f173fd9cf174498f35106319f8
burak zafer ozcan <ozcanburakzafer@gmail.com>
2024-10-16 23:58:12 +0300

    char data inputs from the keyboard was covered

--- 01-veriables_and_data_entry/obj/Debug/net8.0/01-veriables_and_data_entry.AssemblyInfo.cs
+++ 01-veriables_and_data_entry/obj/Debug/net8.0/01-veriables_and_data_entry.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("01-veriables_and_data_entry")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e2849b9f37f0e06156ae36336fdd4c1b58c983f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ee226c0bbacb4a9559407d344fa82df4aef3e1f")]
 [assembly: System.Reflection.AssemblyProductAttribute("01-veriables_and_data_entry")]
 [assembly: System.Reflection.AssemblyTitleAttribute("01-veriables_and_data_entry")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
