986fac66db4e9ede2240c423a8b8afbce99dbee8
burak zafer ozcan <ozcanburakzafer@gmail.com>
2024-10-18 23:24:38 +0300

    Use of for loop with decision structures

--- 03-loops/Program.cs
+++ 03-loops/Program.cs
@@ -24,6 +24,45 @@ namespace _03_loops
             {
                 Console.WriteLine("Yaşasın Şeriat!");
             }
+            Console.WriteLine("------------------------");
+
+            #endregion
+
+
+
+            #region For Döngüsü ile Karar Yapıları
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 5 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.WriteLine("------------------------");
+            int totalValue = 0;
+            for (int i = 1; i < 20; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    totalValue += i;
+                    Console.WriteLine(i);
+                }
+            }
+            Console.WriteLine("toplam : " + totalValue);
+            Console.WriteLine("------------------------");
+
+
+            int count = 0;
+            for (int i = 1; i <= 50; i++)
+            {
+                if (i % 7 == 0)
+                {
+                    count++;
+                    Console.WriteLine(i);
+                }
+            }
+            Console.WriteLine("count değeri adedi : " + count);
+
             #endregion
 
             Console.Read();
--- 03-loops/obj/Debug/net8.0/03-loops.AssemblyInfo.cs
+++ 03-loops/obj/Debug/net8.0/03-loops.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("03-loops")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9d0bd1c8c3ac06676c84e086832820137cfa190")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+731e2119900662d3732644b09dbf9fbc4231802c")]
 [assembly: System.Reflection.AssemblyProductAttribute("03-loops")]
 [assembly: System.Reflection.AssemblyTitleAttribute("03-loops")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
