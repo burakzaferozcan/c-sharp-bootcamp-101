731e2119900662d3732644b09dbf9fbc4231802c
burak zafer ozcan <ozcanburakzafer@gmail.com>
2024-10-18 23:18:43 +0300

    for loop usage

---
+++ 03-loops/Program.cs
@@ -0,0 +1,32 @@
+﻿
+namespace _03_loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region For Döngüsü
+            for (int i = 0; i <= 5; i++)
+            {
+                Console.WriteLine("" + i);
+            }
+            Console.WriteLine("------------------------");
+
+            for (int i = 3; i <= 50; i += 3)
+            {
+                Console.WriteLine("" + i);
+            }
+            Console.WriteLine("------------------------");
+
+            Console.Write("Lütfen ekrana yazılmasını istediğiniz adedi giriniz : ");
+            int finishValue = int.Parse(Console.ReadLine());
+            for (int i = 0; i <= finishValue; i++)
+            {
+                Console.WriteLine("Yaşasın Şeriat!");
+            }
+            #endregion
+
+            Console.Read();
+        }
+    }
+}
--- 03-loops/obj/Debug/net8.0/03-loops.AssemblyInfo.cs
+++ 03-loops/obj/Debug/net8.0/03-loops.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("00-main_subjects")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("03-loops")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9d0bd1c8c3ac06676c84e086832820137cfa190")]
-[assembly: System.Reflection.AssemblyProductAttribute("00-main_subjects")]
-[assembly: System.Reflection.AssemblyTitleAttribute("00-main_subjects")]
+[assembly: System.Reflection.AssemblyProductAttribute("03-loops")]
+[assembly: System.Reflection.AssemblyTitleAttribute("03-loops")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // MSBuild WriteCodeFragment sınıfı tarafından oluşturuldu.
