a1f2f41e5043ba869079996f3be97ae3b841d277
burak zafer ozcan <ozcanburakzafer@gmail.com>
2024-10-20 19:35:46 +0300

    create string and integer array

---
+++ 05-arrays/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace _05_arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Temel Dizi Örnekleri
+            string[] colors = new string[4];
+            colors[0] = "sarı";
+            colors[1] = "kırmızı";
+            colors[2] = "beyaz";
+            colors[3] = "yeşil";
+            Console.WriteLine(colors[0]);
+            Console.WriteLine(colors[1]);
+            Console.WriteLine(colors[2]);
+            Console.WriteLine(colors[3]);
+            Console.WriteLine("-----------------------------");
+
+            string[] cities = new string[5];
+            cities[0] = "milano";
+            cities[1] = "budapeşte";
+            cities[2] = "lyon";
+            cities[3] = "ankara";
+            cities[4] = "kahire";
+            Console.WriteLine(cities[4]);
+            Console.WriteLine("-----------------------------");
+
+            int[] numbers = new int[10];
+            numbers[0] = 50;
+            numbers[1] = 48;
+            numbers[2] = 698;
+            numbers[3] = 24;
+            numbers[4] = 748;
+
+            Console.WriteLine(numbers[4]);
+            Console.WriteLine(numbers[5]);
+            Console.WriteLine("-----------------------------");
+
+            #endregion
+
+            Console.Read();
+        }
+    }
+}
--- 05-arrays/obj/Debug/net8.0/05-arrays.AssemblyInfo.cs
+++ 05-arrays/obj/Debug/net8.0/05-arrays.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("00-main_subjects")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("05-arrays")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+027fa778c2cba06c7ffd89cba001fe74a968a79b")]
-[assembly: System.Reflection.AssemblyProductAttribute("00-main_subjects")]
-[assembly: System.Reflection.AssemblyTitleAttribute("00-main_subjects")]
+[assembly: System.Reflection.AssemblyProductAttribute("05-arrays")]
+[assembly: System.Reflection.AssemblyTitleAttribute("05-arrays")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // MSBuild WriteCodeFragment sınıfı tarafından oluşturuldu.
