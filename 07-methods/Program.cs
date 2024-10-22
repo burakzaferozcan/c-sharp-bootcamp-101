c9386dd8479227cec2fb71783d2d16107f826cc6
burak zafer ozcan <ozcanburakzafer@gmail.com>
2024-10-22 13:32:44 +0300

    Parameterless methods that return a backward value

--- 07-methods/Program.cs
+++ 07-methods/Program.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.Globalization;
+
 namespace _07_methods
 {
     internal class Program
@@ -26,7 +28,7 @@ namespace _07_methods
             Console.WriteLine("-----------------------");
             #endregion
 
-            #region Geriye değer döndürmeyen parametreli metotlar
+            #region Geriye değer döndürmeyen string parametreli metotlar
             void writeMethod(string customerName)
             {
                 Console.WriteLine(customerName);
@@ -40,12 +42,48 @@ namespace _07_methods
             {
                 Console.WriteLine(name + " " + surname);
             }
+
             string customerName = "Burak Zafer";
             string customerSurname = "Özcan";
             customerCard(customerName, customerSurname);
             Console.WriteLine("-----------------------");
 
 
+
+            #endregion
+
+            #region Geriye değer döndürmeyen integer parametreli metotlar
+
+            void sum2(int number1, int number2, int number3)
+            {
+                int result = number1 + number2 + number3;
+                Console.WriteLine(result);
+            }
+            sum2(10, 20, 30);
+
+            Console.WriteLine("-----------------------");
+            #endregion
+
+
+            #region Geriye değer döndüren parametresiz metotlar
+            string CustomerName()
+            {
+                return "burak";
+            }
+            CustomerName();
+            Console.WriteLine(CustomerName());
+            Console.WriteLine("-----------------------");
+
+            string StudentCard()
+            {
+                string name = "burak";
+                string surName = "özcan";
+                return name + " " + surName;
+            }
+            Console.WriteLine(StudentCard());
+            string student = StudentCard();
+            Console.WriteLine(student);
+            Console.WriteLine("-----------------------");
 
             #endregion
 
